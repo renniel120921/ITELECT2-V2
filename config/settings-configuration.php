233cd4beebebe6214a215306828cb1b1cfdd25a4
Renniel Salazar <rennielsalazar948@gmail.com>
2025-04-14 07:17:39 +0800

    Initial commit

---
+++ config/settings-configuration.php
@@ -0,0 +1,17 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+if (empty($_SESSION['csrf_token'])) {
+    $csrf_token = bin2hex(random_bytes(32));
+    $_SESSION['csrf_token'] = $csrf_token;
+}else{
+    $csrf_token = $_SESSION['csrf_token'];
+}
+?>
