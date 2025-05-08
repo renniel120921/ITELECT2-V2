409589f6ca47f685bf45370fc6aea98f5fdb50cf
Renniel Salazar <rennielsalazar948@gmail.com>
2025-05-08 19:00:36 +0800

    push main git

--- index.php
+++ index.php
@@ -7,27 +7,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login & Register</title>
     <link rel="stylesheet" href="src/css/login.css">
 </head>
 <body>
-    <h1>SIGN IN</h1>
-    <form action="dashboard/admin/authentication/admin-class.php" method="POST">
-        <input type="hidden" name="csrf_token" value="<?php echo $csrf_token; ?>">
-        <input type="email" name="email" placeholder="Enter Email" required> <br>
-        <input type="password" name="password" placeholder="Enter Password" required> <br>
-        <button type="submit" name="btn-signin">SIGN IN</button>
-        <p><a href="forgot-password.php">Forgot Password</a></p>
-        <p><a href="reset-password.php">Reset Password</a></p>
-    </form>
+    <div class="form-container">
 
-    <h1>REGISTRATION</h1>
-    <form action="dashboard/admin/authentication/admin-class.php" method="POST">
-        <input type="hidden" name="csrf_token" value="<?php echo $csrf_token; ?>">
-        <input type="text" name="username" placeholder="Enter Username" required> <br>
-        <input type="email" name="email" placeholder="Enter Email" required> <br>
-        <input type="password" name="password" placeholder="Enter Password" required> <br>
-        <button type="submit" name="btn-signup">SIGN UP</button>
-    </form>
+        <!-- Sign In -->
+        <div class="form-box">
+            <h1>SIGN IN</h1>
+            <form action="dashboard/admin/authentication/admin-class.php" method="POST">
+                <input type="hidden" name="csrf_token" value="<?php echo $csrf_token; ?>">
+                <input type="email" name="email" placeholder="Enter Email" required>
+                <input type="password" name="password" placeholder="Enter Password" required>
+                <button type="submit" name="btn-signin">SIGN IN</button>
+                <p><a href="forgot-password.php">Forgot Password</a></p>
+                <p><a href="reset-password.php">Reset Password</a></p>
+            </form>
+        </div>
+
+        <!-- Registration -->
+        <div class="form-box">
+            <h1>REGISTRATION</h1>
+            <form action="dashboard/admin/authentication/admin-class.php" method="POST">
+                <input type="hidden" name="csrf_token" value="<?php echo $csrf_token; ?>">
+                <input type="text" name="username" placeholder="Enter Username" required>
+                <input type="email" name="email" placeholder="Enter Email" required>
+                <input type="password" name="password" placeholder="Enter Password" required>
+                <button type="submit" name="btn-signup">SIGN UP</button>
+            </form>
+        </div>
+
+    </div>
 </body>
 </html>
