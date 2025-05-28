71d507ea3bc52d592a03fbf040cee80fa6bc6e80
Renniel Salazar <rennielsalazar948@gmail.com>
2025-05-28 17:58:40 +0800

    push main git

--- reset-password.php
+++ reset-password.php
@@ -1,5 +1,5 @@
 <?php
-    include_once 'config/settings-configuration.php';
+    include_once(__DIR__ . '/../../config/settings-configuration.php');
 
     // Optional: Get the reset token from the URL
     $token = $_GET['token'] ?? '';
