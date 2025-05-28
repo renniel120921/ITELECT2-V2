3819756ec22a5d4fecb124412afb8e140cef7920
Renniel Salazar <rennielsalazar948@gmail.com>
2025-05-28 10:41:05 +0800

    push main git

--- dashboard/admin/authentication/forgot-password-handler.php
+++ dashboard/admin/authentication/forgot-password-handler.php
@@ -3,7 +3,6 @@ session_start();
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '../../../PHPMailer/src/Exception.php';
 require '../../../PHPMailer/src/PHPMailer.php';
 require '../../../PHPMailer/src/SMTP.php';
 include_once '/../../../database/dbconnection.php';;
